2c57b6e0c87a2ed52ccee40c44febeb5e828be80
pmleconte <pascal.leconte@conseilgouz.com>
2022-09-08 19:03:46 +0200

    2.0.4
    J 4 : setTemplate expects an object

--- packages/plg_system_cgstyle_j4/cgstyle.php
+++ packages/plg_system_cgstyle_j4/cgstyle.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @package CG template switcher Module
- * @version 2.0.0
+ * @version 2.0.4
  * @subpackage  system.cg_style
  *
- * @copyright   Copyright (C) 2021 Conseilgouz. All rights reserved.
+ * @copyright   Copyright (C) 2022 Conseilgouz. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  *
  */
@@ -23,14 +23,14 @@ class PlgSystemCGStyle extends CMSPlugin
 		if ($cookieValue) {
 			$db = Factory::getDbo();
 			$query = $db->getQuery(true);
-			$query->select('template, params');
+			$query->select('*');
 			$query->from('#__template_styles');
 			$query->where('client_id = 0');
 			$query->where('id = ' . (int)$cookieValue);
 			$db->setQuery($query);
 			$style = $db->loadObject();
 			if ($style != null) {
-				$app->setTemplate( $style->template, $style->params );
+				$app->setTemplate( $style); // 4.0 : expect an object
 			}
 		}
     }
