80d2f90c152b1bed45c6d03a9a9c152d18623f3a
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-02-02 19:09:24 +0100

    2.2.1

--- packages/plg_system_cgstyle_j4/src/Extension/Cgstyle.php
+++ packages/plg_system_cgstyle_j4/src/Extension/Cgstyle.php
@@ -1,9 +1,10 @@
 <?php
 /**
  * @package CG template switcher Module
+ * @version 2.1.0
  * @subpackage  system.cg_style
  *
- * @copyright   Copyright (C) 2025 Conseilgouz. All rights reserved.
+ * @copyright   Copyright (C) 2023 Conseilgouz. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  *
  */
@@ -17,6 +18,8 @@ use Joomla\CMS\Version;
 use Joomla\Database\DatabaseAwareTrait;
 use Joomla\Event\SubscriberInterface;
 use YOOtheme\Theme\Joomla\ThemeLoader;
+use YOOtheme\Event;
+use YOOtheme\Config;
 
 use function YOOtheme\app;
 
@@ -52,10 +55,13 @@ final class Cgstyle extends CMSPlugin implements SubscriberInterface
                 $version = substr($j->getShortVersion(), 0, 1);
                 if ($version >= "4") { // Joomla 4 and higher
 
-                    $app->setTemplate($style);
+                    $ret = $app->setTemplate($style);
 
                     if (strpos($style->template, 'yootheme') === 0) {
+                        $config = app(Config::class);
                         app()->call([ThemeLoader::class, 'initTheme']);
+                        $config->set('theme.id', $style->id);
+                        Event::emit('theme.head');
                     }
                     if (strpos($style->template, 'astroid') === 0) {
                         \Astroid\Framework::getTemplate($style->id);
