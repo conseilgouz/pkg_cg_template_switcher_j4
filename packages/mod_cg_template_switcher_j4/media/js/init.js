60e6f7e945c79fc5fe786ee09bc5454e6f94441c
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-02-08 09:59:24 +0100

    2.2.3

--- packages/mod_cg_template_switcher_j4/media/js/init.js
+++ packages/mod_cg_template_switcher_j4/media/js/init.js
@@ -5,70 +5,88 @@
  * @author ConseilGouz 
  * 
  **/
-var options;
+var cgswitchmod = [];
 var CG_TS_Images = new Array();
 document.addEventListener('DOMContentLoaded', function() {
 	if (typeof Joomla === 'undefined' || typeof Joomla.getOptions === 'undefined') {
 		console.error('Joomla.getOptions not found!\nThe Joomla core.js file is not being loaded.');
 		return;
 	}
-	options = Joomla.getOptions('mod_cg_template_switcher');
-	if (typeof options !== 'undefined' ) {
-			go_switch(options);
-	}
+    mains = document.querySelectorAll('.cg_ts_form');
+    for(var i=0; i<mains.length; i++) {
+        let $one = mains[i];
+        switchid = $one.getAttribute("data");
+        cgswitchmod[switchid] = Joomla.getOptions('mod_cg_template_switcher_'+switchid);
+        if (typeof cgswitchmod[switchid] !== 'undefined' ) {
+			go_switch(cgswitchmod[switchid]);
+        }
+    }
 });
-function go_switch(options) {
-	btn_ok = document.getElementById("CG_TS_OKBtn");
+function go_switch(cgswoptions) {
+	let btn_ok = document.getElementById("CG_TS_OKBtn_"+cgswoptions.id);
 	if (btn_ok) {
 		btn_ok.addEventListener('click',function() {
-		sortValue = document.querySelector("#CG_TS_Select").selectedOptions[0].value;
-		CG_TS_Cookie(sortValue);
+            id = this.getAttribute('data');
+            sortValue = document.querySelector("#CG_TS_Select_"+id).selectedOptions[0].value;
+            CG_TS_Cookie(id,sortValue);
 		});
 	}
-	btn_cancel = document.getElementById("CG_TS_CancelBtn");
+	btn_cancel = document.getElementById("CG_TS_CancelBtn_"+cgswoptions.id);
 	if (btn_cancel) {
 		btn_cancel.addEventListener('click',function() {
-		CG_TS_Cookie_Del()
+            id = this.getAttribute('data');
+            CG_TS_Cookie_Del(id);
 		});
 	}
-	if (options.showpreview == 'true') {
-		var templates = options.templates;
+	if (cgswoptions.showpreview == 'true') {
+		var templates = cgswoptions.templates;
 		for (var k in templates) {
 			CG_TS_Images[k] = new Image(templates[k]['width'],templates[k]['height']);
 			CG_TS_Images[k].src = templates[k]['src'];
 			CG_TS_Images[k].preview = templates[k]['preview'];
 		}
 	}
-	document.getElementById("CG_TS_Select").addEventListener( 'change', function(){
-			sortValue = this.selectedOptions[0].value;		
-			if (options.showpreview == 'true') {
-				CG_TS_ImageShow(sortValue);
-			}
-			if (options.autoswitch == 'true') {
-				CG_TS_Cookie(sortValue);
-			}
+	document.getElementById("CG_TS_Select_"+cgswoptions.id).addEventListener( 'change', function(){
+        id = this.parentNode.getAttribute('data');
+		sortValue = this.selectedOptions[0].value;
+		if (cgswoptions.showpreview == 'true') {
+			CG_TS_ImageShow(id,sortValue);
+		}
+		if (cgswoptions.autoswitch == 'true') {
+			CG_TS_Cookie(id,sortValue);
+		}
+        mod  = document.querySelectorAll('.CG_TS_Select');
+        if (mod.length > 1) { // multiple modules : update all modules
+            for (let i = 0; i < mod.length; i++) {
+                for (let j = 0; j < mod[i].length; j++) {
+                    if (mod[i][j].value == sortValue) { 
+                        mod[i].selectedIndex = j;
+                    }
+                }
+            }
+        }
 	});
 }
 
-function CG_TS_ImageShow(s) {
+function CG_TS_ImageShow(id,s) {
 	if (CG_TS_Images[s]) {
-		document.getElementById("CG_TS_Switcher").innerHTML = decodeURIComponent("%3C")+'img src="'+CG_TS_Images[s].preview+'" id="CG_TS_Img"'+decodeURIComponent("%3E");
+		document.getElementById("CG_TS_Switcher_"+id).innerHTML = decodeURIComponent("%3C")+'img src="'+CG_TS_Images[s].preview+'" id="CG_TS_Img"'+decodeURIComponent("%3E");
 	} else {
-		document.getElementById("CG_TS_Switcher").innerHTML = options.noimage;
+		document.getElementById("CG_TS_Switcher_"+id).innerHTML = cgswitchmod[id].noimage;
 	}
-	document.getElementById("CG_TS_SHOW").style ="display:block"; // show image (hidden when starting)
+	document.getElementById("CG_TS_SHOW_"+id).style ="display:block"; // show image (hidden when starting)
 }
-function CG_TS_Cookie_Del() {
+function CG_TS_Cookie_Del(id) {
 	$secure = "";
 	if (window.location.protocol == "https:") $secure="secure;"; 
 	document.cookie = "cg_template=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;samesite=lax;"+$secure;
-	document.getElementById('cg_ts_form').submit();
+	document.getElementById('cg_ts_form_'+id).submit();
 }
-function CG_TS_Cookie(b) {
+function CG_TS_Cookie(id,b) {
 	var expires = "";
-	if (options.cookie_duration > 0) {
+	if (cgswitchmod[id].cookie_duration > 0) {
 		var date = new Date();
-		date.setTime(date.getTime()+(parseInt(options.cookie_duration)*24*60*60*1000));
+		date.setTime(date.getTime()+(parseInt(cgswitchmod[id].cookie_duration)*24*60*60*1000));
 		var expires = "; expires="+date.toGMTString();
 	}
 	$secure = "";
@@ -83,6 +101,6 @@ function CG_TS_Cookie(b) {
             }
         }
     }
-	document.getElementById('cg_ts_form').submit();
+	document.getElementById('cg_ts_form_'+id).submit();
 }
 
--- packages/mod_cg_template_switcher_j4/tmpl/default.php
+++ packages/mod_cg_template_switcher_j4/tmpl/default.php
@@ -44,8 +44,9 @@ if ($params->get('showpreview', 'true') == 'true') {
     }
 }
 $document->addScriptOptions(
-    'mod_cg_template_switcher',
-    array('cookie_duration' => $params->get('cookie_duration', 0),'showpreview' => $params->get('showpreview', 'true'),
+    'mod_cg_template_switcher_'.$module->id,
+    array('id' => $module->id,
+          'cookie_duration' => $params->get('cookie_duration', 0),'showpreview' => $params->get('showpreview', 'true'),
           'autoswitch' => $params->get('autoswitch', 'false'),
           'noimage' => Text::_('NOIMAGE'),'templates' => $templates_js)
 );
@@ -86,19 +87,19 @@ if ($user->id) {
 }
 ?>
 
-<form id="cg_ts_form" method="post" style="border:none" >
-	<div id="CG_TS_SHOW" style="margin:6px 0 0 0;padding:0;border:none;background:none;overflow:hidden;display:none">
-		<div id="CG_TS_Switcher" style="padding:0;border:none;background:none;text-align:center;vertical-align:middle">
+<form id="cg_ts_form_<?php echo $module->id;?>" class="cg_ts_form" data="<?php echo $module->id;?>" method="post" style="border:none" >
+	<div id="CG_TS_SHOW_<?php echo $module->id;?>" style="margin:6px 0 0 0;padding:0;border:none;background:none;overflow:hidden;display:none">
+		<div id="CG_TS_Switcher_<?php echo $module->id;?>" style="padding:0;border:none;background:none;text-align:center;vertical-align:middle">
 		</div>
 	</div>
-	<div id="CG_TS_CHOICE" style="margin:6px 0;padding:0;border:none;background:none;overflow:hidden">
-		<div id="CG_TS_THUMBNAIL" style="padding:0;border:none;background:none;text-align:center">
-			<?php echo HTMLHelper::_('select.genericlist', $templates->options, 'template', "class=\"inputbox\" style=\"margin:0\"", 'value', 'text', $curr_template_idx, 'CG_TS_Select'); ?>
+	<div id="CG_TS_CHOICE_<?php echo $module->id;?>" style="margin:6px 0;padding:0;border:none;background:none;overflow:hidden">
+		<div id="CG_TS_THUMBNAIL_<?php echo $module->id;?>" data="<?php echo $module->id;?>" style="padding:0;border:none;background:none;text-align:center">
+			<?php echo HTMLHelper::_('select.genericlist', $templates->options, 'template', "class=\"inputbox CG_TS_Select\" style=\"margin:0\"", 'value', 'text', $curr_template_idx, 'CG_TS_Select_'.$module->id); ?>
 		</div>
 		<?php if ($params->get("autoswitch", "false") == 'false') { // 01.0.14 : autoswitch?>
-		<div id="CG_TS_LIST" style="padding:6px 0 0 0;border:none;background:none;text-align:center">
-			<input id="CG_TS_OKBtn" class="button" type="button" style="margin:0" value="<?php echo Text::_('CGSELECT'); ?>"/>
-			<input id ="CG_TS_CancelBtn" class="button" type="button" style="margin-left:1em" value="<?php echo Text::_('CGCANCEL'); ?>" title="<?php echo Text::_('CGCANCELDESC'); ?>" />
+		<div id="CG_TS_LIST_<?php echo $module->id;?>" style="padding:6px 0 0 0;border:none;background:none;text-align:center">
+			<input id="CG_TS_OKBtn_<?php echo $module->id;?>" data="<?php echo $module->id;?>" class="button" type="button" style="margin:0" value="<?php echo Text::_('CGSELECT'); ?>"/>
+			<input id ="CG_TS_CancelBtn_<?php echo $module->id;?>" data="<?php echo $module->id;?>" class="button" type="button" style="margin-left:1em" value="<?php echo Text::_('CGCANCEL'); ?>" title="<?php echo Text::_('CGCANCELDESC'); ?>" />
 		</div>
 		<?php } ?>
 	</div>
